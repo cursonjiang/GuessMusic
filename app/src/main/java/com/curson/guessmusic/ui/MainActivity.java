5e3f27deff08341d45070216924dd917eea58cfc
cursonjiang <cursonjiang@foxmail.com>
2015-03-16 16:21:45 +0800

    完成TitleBar与唱片布局

---
+++ app/src/main/java/com/curson/guessmusic/ui/MainActivity.java
@@ -0,0 +1,18 @@
+package com.curson.guessmusic.ui;
+
+import android.os.Bundle;
+import android.support.v7.app.ActionBarActivity;
+
+import com.curson.guessmusic.R;
+
+
+public class MainActivity extends ActionBarActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+
+
+}
